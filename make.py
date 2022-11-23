a037f10d2423a832b2be7df25f821d621056371a
Yihsiang Liow <yliow@ccis.edu>
2022-11-23 15:06:01 -0500

    no msg

--- make.py
+++ make.py
@@ -11,6 +11,9 @@ lines = [_.strip() for _ in lines if _.strip() != '']
 
 for line in lines:
     line2 = line.replace('/computer-vision/doc/', '/computer-vision-downloads/cpp/')
+    # makedirs
+    dirs = os.path.split(line2)
+    print("dirs:", dirs)
     cmd = 'cp "%s" "%s"' % (line, line2)
     print(cmd)
     os.system(cmd)
