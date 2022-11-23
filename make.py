9f3521d408bbb9390c8197f6d2996e7ded6a42bf
Yihsiang Liow <yliow@ccis.edu>
2022-11-23 14:24:13 -0500

    no msg

---
+++ make.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+s = r'''
+/home/student/yliow/Documents/work/projects/computer-vision/doc/compiling/one-cpp/makefile	
+/home/student/yliow/Documents/work/projects/computer-vision/doc/compiling/multiple-files/makefile
+'''
+
+import os
+lines = s.split('\n')
+lines = [_.strip() for _ in lines if _.strip() != '']
+
+for line in lines:
+    line2 = line.replace('/computer-vision/doc/', '/computer-vision-downloads/cpp/')
+    cmd = 'cp "%s" "%s"' % (line, line2)
+    print(cmd)
+    os.system(cmd)
+
+
+os.system('git add .; git commit -m"no msg"; git push')
--- makefile
+++ makefile
@@ -1,10 +1,2 @@
 run:
-	cp \
-	/home/student/yliow/Documents/work/projects/computer-vision/doc/compiling/one-cpp/makefile \
-	/home/student/shares/yliow/Documents/work/projects/computer-vision-downloads/cpp/compiling/one-cpp/makefile
-	
-	cp \
-	/home/student/yliow/Documents/work/projects/computer-vision/doc/compiling/multiple-files/makefile \
-	/home/student/shares/yliow/Documents/work/projects/computer-vision-downloads/cpp/compiling/multiple-files/makefile
-
-
+	python make.py
