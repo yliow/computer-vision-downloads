2fd9299136f66d6240279c88a9c0f94b104bab6f
Yihsiang Liow <yliow@ccis.edu>
2022-11-23 15:15:02 -0500

    no msg

--- make.py
+++ make.py
@@ -3,7 +3,12 @@
 s = r'''
 /home/student/yliow/Documents/work/projects/computer-vision/doc/compiling/one-cpp/makefile	
 /home/student/yliow/Documents/work/projects/computer-vision/doc/compiling/multiple-files/makefile
-
+/home/student/shares/yliow/Documents/work/projects/computer-vision/doc/images/baboon.png
+/home/student/shares/yliow/Documents/work/projects/computer-vision/doc/images/baboon-8x8.png
+/home/student/shares/yliow/Documents/work/projects/computer-vision/doc/images/eye.PNG
+/home/student/shares/yliow/Documents/work/projects/computer-vision/doc/images/lake-bled.jpg
+/home/student/shares/yliow/Documents/work/projects/computer-vision/doc/images/pool.png
+/home/student/shares/yliow/Documents/work/projects/computer-vision/doc/images/volcanic-mt-ngauruhoe.jpg
 /home/student/shares/yliow/Documents/work/projects/computer-vision/doc/videos/blooming-flowers.mp4
 '''
 
@@ -12,6 +17,8 @@ lines = s.split('\n')
 lines = [_.strip() for _ in lines if _.strip() != '']
 
 for line in lines:
+    if not os.path.exists(line):
+        raise Exception("ERROR: file %s not found" % line)
     line2 = line.replace('/computer-vision/doc/', '/computer-vision-downloads/cpp/')
     # makedirs
     dirs = os.path.split(line2)[0] + '/'
