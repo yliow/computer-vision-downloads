9604d888f48f3c574b77d909c778fb3bb3bddb07
Yihsiang Liow <yliow@ccis.edu>
2022-11-23 15:09:54 -0500

    no msg

--- make.py
+++ make.py
@@ -3,6 +3,8 @@
 s = r'''
 /home/student/yliow/Documents/work/projects/computer-vision/doc/compiling/one-cpp/makefile	
 /home/student/yliow/Documents/work/projects/computer-vision/doc/compiling/multiple-files/makefile
+
+/home/student/shares/yliow/Documents/work/projects/computer-vision/doc/videos/blooming-flowers.mp4
 '''
 
 import os
@@ -13,7 +15,7 @@ for line in lines:
     line2 = line.replace('/computer-vision/doc/', '/computer-vision-downloads/cpp/')
     # makedirs
     dirs = os.path.split(line2)[0] + '/'
-    print("dirs:", dirs)
+    #print("dirs:", dirs)
     os.makedirs(dirs, exist_ok=True)
     cmd = 'cp "%s" "%s"' % (line, line2)
     print(cmd)
